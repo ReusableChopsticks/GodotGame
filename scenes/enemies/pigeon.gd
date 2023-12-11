eecf6cff5b3456f8e63e3839a2441182417577e1
ReusableChopsticks <76480675+ReusableChopsticks@users.noreply.github.com>
2023-12-11 12:47:22 +1100

    pigeon hopping
    - pigeons hop towards player.
    - birds face correct direction

--- scenes/enemies/pigeon.gd
+++ scenes/enemies/pigeon.gd
@@ -2,66 +2,81 @@ extends Area2D
 
 @onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
 @onready var hop_timer: Timer = $HopTimer
+@onready var sprite: Sprite2D = $PigeonImage
 
 @export var player_stats: Resource
+# how fast the pigeon hops from one point to another
 @export var hop_travel_speed: float = 0.2
 # Min and max time between each hop
 @export var min_hop_interval: float = 0.5
 @export var max_hop_interval: float = 1
 # Max distance of each hop in global units
-@export var max_hop_distance: float = 100
+@export var max_hop_distance: float = 20
+# amount pigeons scatter a bit when choosing where to hop
+@export var pos_variance: int = 10
 
+const hop_height = 10
 
 var path_loaded = false
 var target_reached = false
 
-var i = 0
 
 func _ready():
 	call_deferred("get_next_pos")
 	nav_agent.path_desired_distance = max_hop_distance
 	hop_timer.start(randf() * max_hop_interval)
 
-#func _physics_process(delta):
-	##print(path_loaded)
-	## when path is loaded and target is not reached yet, move the pigeon
-	##nav_agent.target_position = player_stats.player_pos
-	##print(target_reached)
-	##if true:
-	#if (path_loaded and !target_reached):
-		#if (nav_agent.is_connected("path_changed", _on_navigation_agent_2d_path_changed)):
-			#nav_agent.disconnect("path_changed", _on_navigation_agent_2d_path_changed)
-		#
-		#var target_pos = get_next_pos()
-		#var tween = get_tree().create_tween()
-		#tween.tween_property($".", "position", target_pos, hop_speed)
-
 
 func get_next_pos():
 	nav_agent.target_position = player_stats.player_pos
-	return (nav_agent.get_next_path_position() - position).normalized() * max_hop_distance
-	#return to_local(nav_agent.get_next_path_position()).normalized()
-
+	
+#region Alternate pathfinding with set max distance
+# this code will move the pigeon in the general direction in a definable distance
+# instead of just relying on godot's A* algo to generate points
+# use this code instead if you want to control the hop distance precisely
+# however, it also lets pigeons go where they shouldn't (in collidable places)
+	#var direction = (nav_agent.get_next_path_position() - player_stats.player_pos).normalized() * max_hop_distance
+	#return (position - direction)
+#endregion
+	
+	var target = nav_agent.get_next_path_position()
+	target.x += randf_range(-1, 1) * pos_variance
+	target.y += randf_range(-1, 1) * pos_variance
+	
+	return target
 
 func _on_navigation_agent_2d_navigation_finished():
 	target_reached = true
 	print("pigeon reached player")
 
-
 func _on_navigation_agent_2d_path_changed():
 	path_loaded = true
 
-
 func _on_timer_timeout():
-	i += 1
-	print(i)
 	if (path_loaded):
 		if (nav_agent.is_connected("path_changed", _on_navigation_agent_2d_path_changed)):
 			nav_agent.disconnect("path_changed", _on_navigation_agent_2d_path_changed)
 		
 		var target_pos = get_next_pos()
 		var tween = get_tree().create_tween()
+		# move pigeon to new pos
 		tween.tween_property($".", "position", target_pos, hop_travel_speed)
 		
-		# restart hop timer
+		# hop animation
+		$AnimationPlayer.play("bird_hop")
+		face_direction(position.x, target_pos.x, sprite)
+		
+		# restart hop timer to random time
 		hop_timer.start(randf_range(min_hop_interval, max_hop_interval))
+
+
+# face correct directions, assuming facing right is default
+func face_direction(curr_x_pos: float, target_x_pos: float, entity_sprite: Sprite2D, initial_facing_right: bool = true) -> void:
+	# TODO: MAKE THIS INTO FUNCTION
+	# currenty copy and pasted in seagull.
+	# make a base enemy, seagull and pigeon should inherit from it
+	if (curr_x_pos - target_x_pos < 0):
+		# if entity moving right, face right
+		entity_sprite.flip_h = !initial_facing_right
+	else:
+		entity_sprite.flip_h = initial_facing_right
--- scenes/enemies/seagull.gd
+++ scenes/enemies/seagull.gd
@@ -7,12 +7,15 @@
 
 extends Area2D
 
+@onready var sprite: Sprite2D = $SeagullImage
+
 @export var attack_travel_time: float = 2
 @export var attack_extra_distance_percentage: float = 0.5
 @export var player_stats: Resource
 
 @export var max_time_before_hop: int = 1
 @export var max_hop_distance: int = 5
+# how long it takes to hop from one point to another
 var hop_time: float = 0.1
 
 # to avoid seagull continuously locking on to player
@@ -39,16 +42,21 @@ func _on_detect_area_area_entered(_area):
 func fly_towards_player():
 	var tween = get_tree().create_tween().set_parallel(false)
 	tween.connect("finished", on_attack_finished)
+	
 	# Adding this will make the seagull fly <attack_extra_distance_percentage> more past the player
 	var extra_flight = ((player_stats.player_pos - position) * attack_extra_distance_percentage)
 	var target_pos = player_stats.player_pos + extra_flight
+	
+	# face correct direction
+	face_direction(position.x, target_pos.x, sprite, false)
+	
 	# Make seagull fly to target pos with tween
 	tween.tween_property($".", "position", target_pos, attack_travel_time).set_trans(Tween.TRANS_QUAD)
 	
 func on_attack_finished():
 	is_attacking = false
 
-# TODO: add hop animation that plays
+
 func hop_random_direction():
 	# get random distance and angle
 	var hop_dist = randf() * max_hop_distance
@@ -58,9 +66,11 @@ func hop_random_direction():
 	target_pos.y += hop_dist * sin(angle)
 	
 	# move seagull to this point
-	$AnimationPlayer.play("seagull_hop")
+	$AnimationPlayer.play("bird_hop")
 	var tween = get_tree().create_tween()
 	tween.tween_property($".", "position", target_pos, hop_time).set_trans(Tween.TRANS_QUAD)
+	face_direction(position.x, target_pos.x, sprite, false)
+	
 	
 	# restart hop timer
 	$HopTimer.start(randf() * max_time_before_hop)
@@ -69,4 +79,14 @@ func hop_random_direction():
 func _on_hop_timer_timeout():
 	if !is_attacking:
 		hop_random_direction()
-	
+
+# face correct directions, assuming facing right is default
+func face_direction(curr_x_pos: float, target_x_pos: float, entity_sprite: Sprite2D, initial_facing_right: bool = true) -> void:
+	# TODO: MAKE THIS INTO FUNCTION
+	# currenty copy and pasted in seagull.
+	# make a base enemy, seagull and pigeon should inherit from it
+	if (curr_x_pos - target_x_pos < 0):
+		# if entity moving right, face right
+		entity_sprite.flip_h = !initial_facing_right
+	else:
+		entity_sprite.flip_h = initial_facing_right

00e9c632c97b0fa11ca9ed8fa2235dcd465d9e5d
ReusableChopsticks <76480675+ReusableChopsticks@users.noreply.github.com>
2023-12-16 11:55:57 +1100

    basic childenemy movement added
    - they bounce on walls now.
    - removed collisionshape from enemyhitbox component. now collisionshape must be added onto it after linking to an enemy so you can edit the shape

--- scenes/enemies/child_enemy/child_enemy.gd
+++ scenes/enemies/child_enemy/child_enemy.gd
@@ -1,16 +1,32 @@
+@tool
+
 extends CharacterBody2D
 
-@export var speed: int = 200
+@export var speed: int = 100
 
 func _ready():
-	# choose random starting direction
-	var angle = randf_range(0, 2 * PI)
-	velocity = Vector2(sin(angle), cos(angle)).normalized()
+	if not Engine.is_editor_hint():
+		# choose random starting direction
+		var angle = randf_range(0, 2 * PI)
+		velocity = Vector2(sin(angle), cos(angle)).normalized()
 
 
 func _physics_process(delta):
-	move_and_collide(speed * velocity * delta)
+	if not Engine.is_editor_hint():
+		var collision_info = move_and_collide(speed * velocity * delta)
+		if collision_info:
+			velocity = velocity.bounce(collision_info.get_normal())
 
 
 func _on_area_2d_body_entered(body):
 	print(str(body.name) + " entered child enemy")
+
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings: PackedStringArray = []
+	
+	if get_node("SpriteOrienter") == null:
+		warnings.append("Entity requires a SpriteOrienter scene component")
+	if get_node("EnemyHitboxComponent") == null:
+		warnings.append("Entity requires a EnemyHitboxComponent")
+	return warnings

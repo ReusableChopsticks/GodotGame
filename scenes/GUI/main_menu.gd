3b65eccc365bded2385c60fd0f1ec07d5e7fc02f
ReusableChopsticks <76480675+ReusableChopsticks@users.noreply.github.com>
2023-12-20 00:00:01 +1100

    main menu started
    ridiculous main menu animation

---
+++ scenes/GUI/main_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@onready var animator: AnimationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animator.play("start_screen")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass

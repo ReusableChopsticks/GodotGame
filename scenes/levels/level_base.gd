5e1f4bd67c0b55484e5577952aa84f13d50b9a5a
ReusableChopsticks <76480675+ReusableChopsticks@users.noreply.github.com>
2023-12-29 13:27:15 +1100

    tutorial and countdown
    i found out how to do ui animations

--- scenes/levels/level_base.gd
+++ scenes/levels/level_base.gd
@@ -1,14 +1,16 @@
-extends Control
-
-@onready var animator: AnimationPlayer = $AnimationPlayer
-
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	animator.play("start_screen")
+	
+	get_tree().paused = true
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_tutorial_screen_game_started():
+	get_tree().paused = false
